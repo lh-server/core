62a770ab08ee1769c476e571967e0f888af299bf
GuybrushGit <turinpt3@gmail.com>
2017-11-11 10:36:38 +0000

    Fix to Mind Control dispels and resistances (#23)
    * Fix to Mind Control dispels
    
    * Fix to Mind Control dispels
    
    * Fix to dispel resistance check
    
    * Fix to MC never resisting
    
    * Revert "Fix to MC never resisting"
    
    This reverts commit 85f629bef2a05d627619f98e3388eb5838678006.

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -5394,7 +5394,7 @@ SpellCastResult Spell::CheckCast(bool strict)
             }
         }
 
-        if (IsPositiveSpell(m_spellInfo->Id))
+        if (IsPositiveSpell(m_spellInfo->Id, m_caster, target))
             if (target->IsImmuneToSpell(m_spellInfo, target == m_caster))
                 return SPELL_FAILED_TARGET_AURASTATE;
 
@@ -6235,7 +6235,13 @@ SpellCastResult Spell::CheckCast(bool strict)
                     {
                         bool positive = holder->IsPositive();
                         // do not remove positive auras if friendly target
-                        //               negative auras if non-friendly target
+                        // do not remove negative auras if non-friendly target
+                        // when removing charm auras ignore hostile reaction from the charm
+                        if (!friendly_dispel && !positive && IsCharmSpell(holder->GetSpellProto()))
+                            if (CharmInfo *charm = unit_target->GetCharmInfo())
+                                if (FactionTemplateEntry const* ft = charm->GetOriginalFactionTemplate())
+                                    if (charm->GetOriginalFactionTemplate()->IsFriendlyTo(*m_caster->getFactionTemplateEntry()))
+                                        bFoundOneDispell = true;
                         if (positive == friendly_dispel)
                             continue;
                     }
