d524316c73fd612bf0a182211e5d70fdb7f40251
bladez <prithu@iinet.net.au>
2017-12-06 20:07:07 +1100

    Minor TemporarySummon changes & event cleanups (#923)
    * Cleanup Regthar Deathgate properly if the boss cannot be summoned
    Fix despawn time on boss
    Despawn banners on Kromzar death
    
    * Despawn temporary summons via UnSummon if ForcedDespawn is called on them
    
    * Minor refactor
    
    * Use wrapper for Windsor despawn

--- src/game/Objects/Creature.cpp
+++ src/game/Objects/Creature.cpp
@@ -1889,6 +1889,12 @@ void Creature::Respawn()
 
 void Creature::ForcedDespawn(uint32 timeMSToDespawn)
 {
+    if (IsTemporarySummon())
+    {
+        static_cast<TemporarySummon*>(this)->UnSummon(timeMSToDespawn);
+        return;
+    }
+
     if (timeMSToDespawn)
     {
         ForcedDespawnDelayEvent *pEvent = new ForcedDespawnDelayEvent(*this);
