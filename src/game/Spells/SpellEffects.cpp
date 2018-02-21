2920aa3f424741ca973d9ff65dd10862b0ffcf6a
Prithu <prithu@iinet.net.au>
2018-02-21 08:29:21 +0000

    Merge branch 'valentines2' into 'development'
    Valentines event fixes
    
    See merge request lights-hope/server!17

--- src/game/Spells/SpellEffects.cpp
+++ src/game/Spells/SpellEffects.cpp
@@ -4820,6 +4820,7 @@ void Spell::EffectScriptEffect(SpellEffectIndex eff_idx)
                         unitTarget->RemoveAurasDueToSpell(31800); // Icebolt immunity spell
                         unitTarget->RemoveAurasDueToSpell(28522); // Icebolt stun/damage spell
                     }
+                    return;
                 }
                 case 28352:									// Atiesh - Breath of Sargeras
                 {
@@ -4841,6 +4842,7 @@ void Spell::EffectScriptEffect(SpellEffectIndex eff_idx)
                             return;
                         }
                     }
+                    return;
                 }
                 case 26678:                                 // Bag of Candies
                 {
