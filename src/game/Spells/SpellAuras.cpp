226d67dffeac8eb76cc7acd67ce474d41fb4a6f5
GBTW <turinpt3@gmail.com>
2018-03-30 02:19:32 +0100

    Prevent controlling sheeps while under MC

--- src/game/Spells/SpellAuras.cpp
+++ src/game/Spells/SpellAuras.cpp
@@ -2906,8 +2906,9 @@ void Unit::ModPossess(Unit* target, bool apply, AuraRemoveMode m_removeMode)
         p_caster->PossessSpellInitialize();
 
         if (Creature* pTargetCrea = target->ToCreature())
-            if (pTargetCrea->AI()->SwitchAiAtControl())
-                pTargetCrea->AIM_Initialize();
+            if (!pTargetCrea->hasUnitState(UNIT_STAT_CAN_NOT_REACT))
+                if (pTargetCrea->AI()->SwitchAiAtControl())
+                    pTargetCrea->AIM_Initialize();
 
         if (target->IsPlayer() && !caster->IsPlayer())
                 target->ToPlayer()->SetControlledBy(caster);
@@ -2957,17 +2958,23 @@ void Unit::ModPossess(Unit* target, bool apply, AuraRemoveMode m_removeMode)
 
         target->RestoreFaction();
         target->CombatStop(true);
-        target->StopMoving(true);
         target->UpdateControl();
         target->SetWalk(false);
 
         if (Creature* pCreature = target->ToCreature())
         {
-            if (pCreature->AI() && pCreature->AI()->SwitchAiAtControl())
-                pCreature->AIM_Initialize();
+            if (!pCreature->hasUnitState(UNIT_STAT_CAN_NOT_REACT))
+            {
+                target->StopMoving(true);
+                if (pCreature->AI() && pCreature->AI()->SwitchAiAtControl())
+                    pCreature->AIM_Initialize();
 
-            pCreature->AttackedBy(caster);
+                pCreature->AttackedBy(caster);
+            }
         }
+        else
+            target->StopMoving(true);
+
         // cast mind exhaustion on self when the posess possess ends if the creature
         // is death knight understudy (razuvious). 
         // todo: if there is a way to know a possess has ended through scriptAI, fix this.
