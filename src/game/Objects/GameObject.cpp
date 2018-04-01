f1330ef6c625d4d05ae63140f49f8c29dfef4be0
GBTW <turinpt3@gmail.com>
2018-04-02 00:23:00 +0100

    Respawn linked trap with parent

--- src/game/Objects/GameObject.cpp
+++ src/game/Objects/GameObject.cpp
@@ -318,7 +318,12 @@ void GameObject::Update(uint32 update_diff, uint32 /*p_time*/)
                             //we need to open doors if they are closed (add there another condition if this code breaks some usage, but it need to be here for battlegrounds)
                             if (GetGoState() != GO_STATE_READY)
                                 ResetDoorOrButton();
-                        //flags in AB are type_button and we need to add them here so no break!
+                            //flags in AB are type_button and we need to add them here so no break!
+                        case GAMEOBJECT_TYPE_CHEST:
+                        case GAMEOBJECT_TYPE_SPELL_FOCUS:
+                        case GAMEOBJECT_TYPE_GOOBER:
+                            // Respawn linked trap if any exists
+                            RespawnLinkedGameObject();
                         default:
                             if (!m_spawnedByDefault)        // despawn timer
                             {
@@ -1163,6 +1168,34 @@ void GameObject::TriggerLinkedGameObject(Unit* target)
     // found correct GO
     if (trapGO && trapGO->isSpawned())
         trapGO->Use(target);
+}
+
+void GameObject::RespawnLinkedGameObject()
+{
+    uint32 trapEntry = GetGOInfo()->GetLinkedGameObjectEntry();
+
+    if (!trapEntry)
+        return;
+
+    GameObjectInfo const* trapInfo = sGOStorage.LookupEntry<GameObjectInfo>(trapEntry);
+    if (!trapInfo || trapInfo->type != GAMEOBJECT_TYPE_TRAP)
+        return;
+
+    float range = 0.5f;
+
+    // search nearest linked GO
+    GameObject* trapGO = NULL;
+    {
+        // search closest with base of used GO, using max range of trap spell as search radius
+        MaNGOS::NearestGameObjectEntryInObjectRangeCheck go_check(*this, trapEntry, range);
+        MaNGOS::GameObjectLastSearcher<MaNGOS::NearestGameObjectEntryInObjectRangeCheck> checker(trapGO, go_check);
+
+        Cell::VisitGridObjects(this, checker, range);
+    }
+
+    // Respawn the trap
+    if (trapGO && !trapGO->isSpawned())
+        trapGO->Respawn();
 }
 
 GameObject* GameObject::LookupFishingHoleAround(float range)
