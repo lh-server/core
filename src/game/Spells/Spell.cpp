11c7101b6405195ecfc309bfe46358429d2b63df
GBTW <turinpt3@gmail.com>
2018-06-16 23:10:39 +0100

    Fix absorbed Ignite damage

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -281,6 +281,7 @@ Spell::Spell(Unit* caster, SpellEntry const *info, bool triggered, ObjectGuid or
     m_isChannelingVisual = false;
 
     m_applyMultiplierMask = 0;
+    m_absorbed = 0;
 
     // Get data for type of attack
     m_attackType = GetWeaponAttackType(m_spellInfo);
@@ -8219,6 +8220,7 @@ void Spell::ResetEffectDamageAndHeal()
 {
     m_damage = 0;
     m_healing = 0;
+    m_absorbed = 0;
 }
 
 void Spell::SetClientStarted(bool bisClientStarted)
