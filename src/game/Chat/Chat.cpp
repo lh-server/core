6c97cfaee96d113f3db0251541a24dc2f62973fb
gears-lightshope <34231184+gears-lightshope@users.noreply.github.com>
2018-07-25 15:06:32 +1000

    Tabs to spaces
    fix tabs to spaces

--- src/game/Chat/Chat.cpp
+++ src/game/Chat/Chat.cpp
@@ -3502,10 +3502,10 @@ std::string ChatHandler::ExtractPlayerNameFromLink(char** text)
  */
 bool ChatHandler::ExtractPlayerTarget(char** args, Player** player /*= NULL*/, ObjectGuid* player_guid /*= NULL*/, std::string* player_name /*= NULL*/, bool use_extended_response)
 {
-	std::string name = "";
+    std::string name = "";
     if (*args && **args)
     {
-		name = ExtractPlayerNameFromLink(args);
+        name = ExtractPlayerNameFromLink(args);
         if (name.empty())
         {
             SendSysMessage(LANG_PLAYER_NOT_FOUND);
@@ -3546,14 +3546,14 @@ bool ChatHandler::ExtractPlayerTarget(char** args, Player** player /*= NULL*/, O
     // some from req. data must be provided (note: name is empty if player not exist)
     if ((!player || !*player) && (!player_guid || !*player_guid) && (!player_name || player_name->empty()))
     {
-		if (use_extended_response && !name.empty()) {
-			std::string message(GetMangosString(LANG_PLAYER_NOT_FOUND));
-			message.append(" (" + name + ")");
-			SendSysMessage(message.c_str());
-		}
-		else {
-			SendSysMessage(LANG_PLAYER_NOT_FOUND);
-		}
+        if (use_extended_response && !name.empty()) {
+            std::string message(GetMangosString(LANG_PLAYER_NOT_FOUND));
+            message.append(" (" + name + ")");
+            SendSysMessage(message.c_str());
+        }
+        else {
+            SendSysMessage(LANG_PLAYER_NOT_FOUND);
+        }
         SetSentErrorMessage(true);
         return false;
     }
