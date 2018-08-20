5a7f6bbddd72d896f82337257543a4051801f434
Klannoc <42480939+Klannoc@users.noreply.github.com>
2018-08-20 21:42:59 +0000

    Fix Bruiseweed Spawn position z (GUID: 3440)
    Bruiseweed GUID 3440 is spawned floating in mid air (17.5). Fixed the position_z field to be on the ground.

---
+++ sql/migrations/20180820173425_world.sql
@@ -0,0 +1,18 @@
+-e DROP PROCEDURE IF EXISTS add_migration;
+delimiter ??
+CREATE PROCEDURE `add_migration`()
+BEGIN
+DECLARE v INT DEFAULT 1;
+SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20180820173425');
+IF v=0 THEN
+INSERT INTO `migrations` VALUES ('20180820173425');
+-- Add your query below.
+
+UPDATE `gameobject` SET `position_z`=3.727536 WHERE `guid`=3440;
+
+-- End of migration.
+END IF;
+END??
+delimiter ; 
+CALL add_migration();
+DROP PROCEDURE IF EXISTS add_migration;
