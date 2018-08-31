cd6d877bda75371bdc9cb9b1cc36b8d0f0f7f2e5
Filip Jaros <fjaros91@gmail.com>
2018-08-30 23:28:03 -0700

    Blackwing technician pull with group

---
+++ sql/migrations/20180830232306_world.sql
@@ -0,0 +1,19 @@
+DROP PROCEDURE IF EXISTS add_migration;
+delimiter ??
+CREATE PROCEDURE `add_migration`()
+BEGIN
+DECLARE v INT DEFAULT 1;
+SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20180830232306');
+IF v=0 THEN
+INSERT INTO `migrations` VALUES ('20180830232306');
+-- Add your query below.
+
+-- Add Technician in Firemaw room's left alcove group to pull with the rest of the group.
+INSERT INTO `creature_groups` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `flags`) VALUES (`84558`, `84570`, `0`, `0`, `46`);
+
+-- End of migration.
+END IF;
+END??
+delimiter ; 
+CALL add_migration();
+DROP PROCEDURE IF EXISTS add_migration;
